8103908d91508abb5107e97bedcc78e4ed3bd7f3
Prince Kannah <PrinceKannah@u.boisestate.edu>
2017-11-09 13:50:45 -0700

    Polish input button behavnior

--- js/input-renderer.js
+++ js/input-renderer.js
@@ -1,46 +1,97 @@
-const ipc = require('electron').ipcRenderer
-
-/**
- * The general workflow when a button is clicked is to get the button,
- * create a channel to send message to main process then send the message
- * 
- * On click send signal to main process to show file selection dialog box
- * The second string is to ID the button that was pushed
-*/
-
-// OUTPUT DIRECTORY
-const selectOutputDirBtn = document.getElementById('selectOutputDirBtn')
-selectOutputDirBtn.addEventListener('click', function (event) {
-    ipc.send('open-file-dialog', 'selectOutputDirBtn')
-})
-
-// LIGAND
 const selectLigandBtn = document.getElementById('selectLigandBtn')
-selectLigandBtn.addEventListener('click', function (event) {
-    ipc.send('open-file-dialog', 'selectLigandBtn')
-})
+selectLigandBtn.onclick = function () {
+    const useLigandList = document.getElementById('ligandUseList').checked
+    if (useLigandList) {
+        // dialog title, file filters, type of dialog promt (file selection in this case)
+        // input field to populate with selected file
+        showDialog('Select ligand list file', [{
+            name: 'All files',
+            extensions: ['*']
+        }], ['openFile'], 'ligandDisplayArea')
+    } else {
+        showDialog('Select ligand file', [{
+            name: 'Protein Data Bank',
+            extensions: ['pdb']
+        }], ['openFile'], 'ligandDisplayArea')
+    }
+}
 
-// SECONDARY LIGAND
 const selectSecondaryLigandBtn = document.getElementById('selectSecondaryLigandBtn')
-selectSecondaryLigandBtn.addEventListener('click', function (event) {
-    ipc.send('open-file-dialog', 'selectSecondaryLigandBtn')
-})
+selectSecondaryLigandBtn.onclick = function () {
+    const useSecondaryLigandList = document.getElementById('secondaryLigadUseList').checked
+    if (useSecondaryLigandList) {
+        showDialog('Select secondary ligand list file', [{
+            name: 'All files',
+            extensions: ['*']
+        }], ['openFile'], 'secondaryLigandDisplayArea')
+    } else {
+        showDialog('Select secondary ligand file', [{
+            name: 'Protein Data Bank',
+            extensions: ['pdb']
+        }], ['openFile'], 'secondaryLigandDisplayArea')
+    }
+}
 
-// RECPETOR
 const selectReceptorBtn = document.getElementById('selectReceptorBtn')
-selectReceptorBtn.addEventListener('click', function (event) {
-    ipc.send('open-file-dialog', 'selectReceptorBtn')
-})
+selectReceptorBtn.onclick = function () {
+    const receptorUseList = document.getElementById('receptorUseList').checked
+    if (receptorUseList) {
+        showDialog('Select receptor list file', [{
+            name: 'All files',
+            extensions: ['*']
+        }], ['openFile'], 'receptorDisplayArea')
+    } else {
+        showDialog('Select receptor file', [{
+            name: 'Protein Data Bank',
+            extensions: ['pdb']
+        }], ['openFile'], 'receptorDisplayArea')
+    }
+}
 
-// GRID PARAMETER FILE
 const selectGPFBtn = document.getElementById('selectGPFBtn')
-selectGPFBtn.addEventListener('click', function (event) {
-    ipc.send('open-file-dialog', 'selectGPFBtn')
-})
+selectGPFBtn.onclick = function () {
+    const useList = document.getElementById('gpfUseList').checked
+    if (useList) {
+        showDialog('Select Grid Parameter File list', [{
+            name: 'All files',
+            extensions: ['*']
+        }], ['openFile'], 'gpfDisplayArea')
+    } else {
+        showDialog('Select Grid Parameter File', [{
+            name: 'Grid Parameter File',
+            extensions: ['gpf']
+        }], ['openFile'], 'gpfDisplayArea')
+    }
+}
+
+const selectOutputDirBtn = document.getElementById('selectOutputDirBtn')
+selectOutputDirBtn.onclick = function () {
+    showDialog('Select Grid Parameter File list', [{
+        name: 'All files',
+        extensions: ['*']
+    }], ['openDirectory', 'createDirectory'], 'outputDirDisplay')
+    // if on macOS 'createDirectory' allows directory creation from within the dialog
+}
 
 /**
- * The main process will respond 
+ * Show native file/directory selection
+ * @param {*String} dialogTitle Display title of the prompt
+ * @param {*String[]} dialogFilters Electron FileFilters
+ * @param {*String[]} dialogProperties Electron showOpenDialog properties object
+ * @param {*String} displayArea Input fieldd to populate with the selected file/directory name
  */
-ipc.on('selected-directory', function (event, path) {
-    document.getElementById('outputDirDisplay').value = `${path}`
-})
+function showDialog(dialogTitle, dialogFilters, dialogProperties, displayArea) {
+    const {
+        dialog
+    } = require('electron').remote
+    dialog.showOpenDialog({
+        title: dialogTitle,
+        filters: dialogFilters,
+        properties: dialogProperties
+    }, function (files) {
+        document.getElementById(displayArea).value = `${files}`
+    })
+
+    // destroy dialog prompt to be safe
+    dialog = null
+}
